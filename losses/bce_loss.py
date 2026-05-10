3c23493da3d5948c27c148a32c6e2549bdd90a8a
dmytro <dmytro19022006@gmail.com>
2026-05-10 22:09:41 +0300

    BCE loss

---
+++ losses/bce_loss.py
@@ -0,0 +1,20 @@
+from .base_gan_loss import BaseGANLoss
+import torch.nn as nn
+import torch
+
+
+class BCEGANLoss(BaseGANLoss):
+    """Binary cross entropy loss with non-saturating generator's loss."""
+    def __init__(self) -> None:
+        self.bce = nn.BCEWithLogitsLoss()
+
+    def D_loss(self, real: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
+        real_targets = torch.ones_like(real)
+        fake_targets = torch.zeros_like(fake)
+        real_loss = self.bce(real, real_targets)
+        fake_loss = self.bce(fake, fake_targets)
+        return real_loss + fake_loss
+
+    def G_loss(self, fake: torch.Tensor) -> torch.Tensor:
+        targets = torch.ones_like(fake)
+        return self.bce(fake, targets)
