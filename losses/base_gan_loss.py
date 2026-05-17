243e8a81d5c87c8a0311529908b9a7a00310ebea
dmytro <dmytro19022006@gmail.com>
2026-05-17 13:06:10 +0300

    minor fixes

--- losses/base_gan_loss.py
+++ losses/base_gan_loss.py
@@ -6,10 +6,10 @@ class BaseGANLoss(ABC):
     """Base class for GAN loss implementations."""
     @abstractmethod
     def D_loss(self, real: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
-        """Calclulate discriminator loss between real and fake (generated) images."""
+        """Calculate discriminator loss between real and fake (generated) images."""
         pass
     
     @abstractmethod
     def G_loss(self, fake: torch.Tensor) -> torch.Tensor:
-        """Calclulate generator loss on a fake image."""
+        """Calculate generator loss on a fake image."""
         pass
--- losses/hinge_loss.py
+++ losses/hinge_loss.py
@@ -1,13 +1,9 @@
 from .base_gan_loss import BaseGANLoss
-import torch.nn as nn
 import torch
 
 
 class HingeLoss(BaseGANLoss):
     """Hinge loss, aiming to maximize the margin between the scores of real and generated data."""
-    def __init__(self) -> None:
-        self.bce = nn.BCEWithLogitsLoss()
-
     def D_loss(self, real: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
         real_loss = torch.mean(torch.maximum(0, 1 - real))
         fake_loss = torch.mean(torch.maximum(0, 1 + fake))
