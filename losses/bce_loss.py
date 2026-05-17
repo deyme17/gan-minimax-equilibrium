ca93948a29051e38ebcfaa54932f6337ee87a553
dmytro <dmytro19022006@gmail.com>
2026-05-17 13:44:28 +0300

    label smoothing

--- losses/bce_loss.py
+++ losses/bce_loss.py
@@ -5,11 +5,12 @@ import torch
 
 class BCEGANLoss(BaseGANLoss):
     """Binary cross entropy loss with non-saturating generator's loss."""
-    def __init__(self) -> None:
+    def __init__(self, label_smoothing: float = 0):
+        self.real_label = 1 - label_smoothing
         self.bce = nn.BCEWithLogitsLoss()
 
     def D_loss(self, real: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
-        real_targets = torch.ones_like(real)
+        real_targets = torch.full_like(real, self.real_label)
         fake_targets = torch.zeros_like(fake)
         real_loss = self.bce(real, real_targets)
         fake_loss = self.bce(fake, fake_targets)
