819395bd832e71ed442e5ab00cdb088a11076733
dmytro <dmytro19022006@gmail.com>
2026-05-20 00:04:18 +0300

    label_smoothing in hinge

--- losses/hinge_loss.py
+++ losses/hinge_loss.py
@@ -4,8 +4,11 @@ import torch
 
 class HingeLoss(BaseGANLoss):
     """Hinge loss, aiming to maximize the margin between the scores of real and generated data."""
+    def __init__(self, label_smoothing: float = 0):
+        self.real_label = 1 - label_smoothing
+    
     def D_loss(self, real: torch.Tensor, fake: torch.Tensor) -> torch.Tensor:
-        real_loss = torch.mean(torch.clamp(1 - real, min=0))
+        real_loss = torch.mean(torch.clamp(self.real_label - real, min=0))
         fake_loss = torch.mean(torch.clamp(1 + fake, min=0))
         return real_loss + fake_loss
     
